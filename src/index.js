53de5deb9e1e281b11c3643db36d0d7ae65a39b0
ishaban.flash <ishaban.flash@gmail.com>
2017-01-12 22:23:28 +0300

    Rename getTokens to getJSON method, to simplify migration from async library. Add ability to define custom `generateScopedName` function.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import scopeGenerator from './scopeGenerator'
 import localByDefault from 'postcss-modules-local-by-default'
 import scope from 'postcss-modules-scope'
 import postcss from 'postcss'
+import genericNames from 'generic-names';
 
-scope.generateScopedName = scopeGenerator
+function getScopedNameGenerator(opts) {
+  const scopedNameGenerator = opts.generateScopedName || scopeGenerator;
+
+  if (typeof scopedNameGenerator === 'function') return scopedNameGenerator;
+  return genericNames(scopedNameGenerator, {context: process.cwd()});
+}
 
 export default postcss.plugin('postcss-css-modules', (opts = {}) => {
-  const plugins = [localByDefault, scope]
-  const parser = new Parser()
-  
+  const plugins = [localByDefault, scope];
+  const parser = new Parser();
+
+  scope.generateScopedName = getScopedNameGenerator(opts);
+
   return (css, result) => {
-    const styles = postcss(plugins.concat(parser.plugin)).process(css).css
-    opts.getTokens(parser.exportTokens)
+    const styles = postcss(plugins.concat(parser.plugin)).process(css).css;
+    if (opts.getJSON != undefined) {
+      opts.getJSON(parser.exportTokens)
+    }
   }
 })
